f8a38f83e3330ba093a67d81ab01cd685bd4f36d
Isaac Miguel <isaacmiguel@gmail.com>
2023-12-05 10:34:11 -0300

    Prettier (#3)
    * add prettier dependencies and script
    
    * run format

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react"
 
-import { users } from './data'
+import { users } from "./data"
 
 function App() {
   return (
     <>
       <h1>Probando github actions!!!</h1>
-      {
-        users.map(u => <p>{u.name}, edad: {u.age}</p>)
-      }
+      {users.map((u) => (
+        <p>
+          {u.name}, edad: {u.age}
+        </p>
+      ))}
     </>
   )
 }
