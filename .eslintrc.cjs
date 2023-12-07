87a3a7541bb4dedf899e3a6515ae3c325e105a62
isaacmiguel <isaacmiguel@gmail.com>
2023-12-07 18:51:19 -0300

    chore(github actions): fix linter and prettier changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: ["semistandard"],
+  extends: ["semistandard", "plugin:react/recommended"],
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "space-before-function-paren": ["error", "never"],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always",
+        objects: "always",
+        imports: "never",
+        exports: "never",
+        functions: "never",
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 }
