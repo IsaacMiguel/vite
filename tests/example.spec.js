5b41bb4740c790f74c69d7bb12b8077bf7b13085
isaacmiguel <isaacmiguel@gmail.com>
2024-02-15 15:46:19 -0300

    style(formating): format code after changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,9 +2,9 @@
 const { test, expect } = require("@playwright/test")
 
 test("has title", async ({ page }) => {
-  await page.goto('http://localhost:5173')
+  await page.goto("http://localhost:5173")
 
-  expect(page.locator('tag=h1').getByText('hi!')).toBeTruthy()
+  expect(page.locator("tag=h1").getByText("hi!")).toBeTruthy()
 
   await expect(page).toHaveTitle("Vite + React")
 })
