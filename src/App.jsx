b0d83479f5b81a3d6827d167547cbddac6ec3518
isaacmiguel <isaacmiguel@gmail.com>
2024-03-12 17:20:31 -0300

    feat(react-axe): accessibility
    adding accessibility library

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { users } from "./data"
 
 function App() {
   return (
-    <>
+    <main role="main">
       <h1>hi!</h1>
       {users.map((u) => (
         <p key={u.id}>
           name: {u.name}, age: {u.age}
         </p>
       ))}
-    </>
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
-// @ts-ignore for document.getElementById('root')
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (process.env.NODE_ENV !== 'production') {
+  // Not For Production
+  import('@axe-core/react').then(axe => {
+    axe.default(React, ReactDOM, 1000)
+    // @ts-ignore for document.getElementById('root')
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  })
+} else {
+  // For Production
+  // @ts-ignore for document.getElementById('root')
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
